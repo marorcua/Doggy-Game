a36226a8843a0a26e25cc68dd334a6de4be0ae25
Mario Ortiz <ortiz.mario22@gmail.com>
2021-04-05 18:06:08 +0200

    reorganized code

--- js/game.js
+++ js/game.js
@@ -1,10 +1,143 @@
 const doggyApp = {
     name: 'HTML5 Canvas doggy app',
     description: 'Game for Ironhack first project',
-    author: 'Guillermo XXX & Mario Ortiz',
+    author: 'Guillermo Muñoz & Mario Ortiz',
     license: undefined,
     version: '1.0.0',
-    canvasDom: undefined,
+    canvas: undefined,
     ctx: undefined,
     canvasSize: { w: undefined, h: undefined },
+
+    FPS: 60,
+    currentLoopIndex: 0,
+    frameCount: 0,
+    frameLimit: 8,
+    currentDirection: 0,
+    MOVEMENT_SPEED: 1,
+    doggyPositionX: undefined,
+    doggyPositionY: undefined,
+    doggyDown: 0,
+    doggyLeft: 1,
+    doggyRight: 2,
+    doggyUp: 3,
+    doggyIni: this.doggyDown,
+    doggyMovement: false,
+
+    keyPresses: {},
+    gameKeys: ["ArrowRight", "ArrowUp", "ArrowDown", "ArrowLeft"],
+
+    doggy: undefined,
+
+    init() {
+        this.canvas = document.getElementById("canvas")
+        this.ctx = this.canvas.getContext("2d")
+        this.setDimensions()
+        this.setEventListeners()
+        this.start()
+
+    },
+
+    setDimensions() {
+        this.canvasSize.w = 600
+        this.canvasSize.h = 900
+        this.doggyPositionX = this.canvasSize.w / 2
+        this.doggyPositionY = this.canvasSize.h - 100
+        //this.canvasDom.setAttribute('width', this.canvasSize.w)
+        //this.canvasDom.setAttribute('height', this.canvasSize.h)
+    },
+
+    setEventListeners() {
+
+        document.onkeydown = e => {
+            this.gameKeys.forEach(elm => {
+                if (e.key === elm) {
+                    this.keyPresses[e.key] = true
+                }
+            });
+        }
+
+        document.onkeyup = e => {
+            this.gameKeys.forEach(elm => {
+                if (e.key === elm) {
+                    this.keyPresses[e.key] = false
+                }
+            });
+        }
+
+    },
+
+    start() {
+        this.reset()
+
+        this.interval = setInterval(() => {
+
+            this.clear()
+            this.movement()
+            this.drawDoggy()
+
+            if (this.doggyMovement) {
+                this.frameCount++
+
+                if (this.frameCount % 8 === 0) {
+
+                    this.currentLoopIndex++
+
+                    if (this.currentLoopIndex >= this.doggy.cycleLoop.length) {
+                        this.currentLoopIndex = 0;
+
+                    }
+
+                }
+
+            }
+
+        }, 1000 / this.FPS)
+    },
+
+    reset() {
+        this.doggy = new Doggy(this.ctx, this.canvas, this.canvasSize, this.currentLoopIndex)
+
+    },
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
+    },
+
+    movement() {
+
+        this.doggyMovement = false
+
+        if (doggyApp.keyPresses.ArrowUp) {
+            this.doggyPositionY -= this.MOVEMENT_SPEED;
+            this.currentDirection = this.doggyUp;
+            this.doggyMovement = true
+
+        } else if (doggyApp.keyPresses.ArrowDown) {
+            this.doggyPositionY += this.MOVEMENT_SPEED;
+            this.currentDirection = this.doggyDown;
+            this.doggyMovement = true
+        }
+        if (doggyApp.keyPresses.ArrowLeft) {
+            this.doggyPositionX -= this.MOVEMENT_SPEED;
+            this.currentDirection = this.doggyLeft;
+            this.doggyMovement = true
+
+        } else if (doggyApp.keyPresses.ArrowRight) {
+            this.doggyPositionX += this.MOVEMENT_SPEED;
+            this.currentDirection = this.doggyRight;
+            this.doggyMovement = true
+
+        }
+
+    },
+
+    createDoggy() {
+        this.doggy = new Doggy(this.ctx, this.canvas, this.canvasSize, this.currentLoopIndex)
+    },
+
+    drawDoggy() {
+        this.doggy.drawDoggyFrame(this.doggy.cycleLoop[this.currentLoopIndex], this.currentDirection, this.doggyPositionX, this.doggyPositionY);
+    }
+
+
 }
